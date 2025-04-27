fc557543060ea7d5db12750110e65ddd11d16057
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-04-27 01:50:25 -0300

    Adicionando tarefas realizadas

--- detector-borda.v
+++ detector-borda.v
@@ -2,9 +2,19 @@ module detector_borda (
     input clk,
     input rst,
     input [1:0] entrada,
-    output [1:0] detector
+    output reg [1:0] detector
 );
     
-//insira seu código aqui
+reg [1:0] entrada_antiga; // Registro para armazenar o valor anterior da entrada
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin // Se reset = 1, zera os registros
+        entrada_antiga = 0;
+        detector = 0;
+    end else begin
+        detector = (~entrada_antiga) & entrada;
+        entrada_antiga = entrada;
+    end
+end
 
 endmodule
